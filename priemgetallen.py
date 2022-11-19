b04fa9efa51b4ea39698ead57af561ac807ac494
bhvoetberg <b.h.voetberg@gmail.com>
2022-11-19 21:27:02 +0100

    Fout in while

--- priemgetallen.py
+++ priemgetallen.py
@@ -1,17 +1,14 @@
 # zeef van erastothenes
 
 lijst = []
-for i in range(2, 101):
+for i in range(2, 30):
     lijst.append(i)
 print(lijst)
 
 i = 0
-for t in range(2,100):
+for t in range(2, 30):
     while i < len(lijst):
-        if lijst[i]%t == 0:
+        if lijst[i] % t == 0:
             lijst.pop(i)
-    i += 1
+        i += 1
     print(lijst)
-
-
-
